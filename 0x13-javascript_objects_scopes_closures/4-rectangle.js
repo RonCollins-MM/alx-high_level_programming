46bb69810169dcee50657328afe2c4217b2bca75
Roncollins MM <muriukiron@gmail.com>
2022-10-04 23:08:36 +0300

    Some more class methods added

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -13,11 +13,11 @@ module.exports = class Rectangle {
   }
 
   rotate () {
-    [this.width, this.length] = [this.length, this.width];
+    [this.width, this.height] = [this.height, this.width];
   }
 
   double () {
     this.width = 2 * this.width;
-    this.lenght = 2 * this.length;
+    this.height = 2 * this.height;
   }
 };
