95561f03d44d62a3b118001ad90d93df481bc28d
Roncollins MM <muriukiron@gmail.com>
2022-10-04 22:44:52 +0300

    Added a class method

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,11 +8,10 @@ module.exports = class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      let str = '';
       for (let j = 0; j < this.widht; j++) {
-        str += 'X';
+        process.stdout.write('X');
       }
-      console.log(str);
+      process.stdout.write('\n');
     }
   }
 };
