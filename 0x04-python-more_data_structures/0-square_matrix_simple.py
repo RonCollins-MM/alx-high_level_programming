56ff66a0bf448c861d0e8754f5b088f6c653c1e6
Roncollins MM <muriukiron@gmail.com>
2022-10-05 19:05:15 +0300

    Broke the code, had to revert

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda x: x**2, matrix))
+    for i in range(len(matrix)):
+	    sq_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+    return sq_matrix
