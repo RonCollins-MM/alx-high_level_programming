642ee7ce538e6eed3947561d46b338ad65f54ce6
Roncollins MM <muriukiron@gmail.com>
2022-10-08 09:36:49 +0300

    Implementation for Student class

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
-"""this module creates a student class"""
-
+"""This module contains a Student class with an attribute to return its
+__dict__ format."""
 
 class Student:
-    """this is the student class"""
+    """Implementation for Student class"""
     def __init__(self, first_name, last_name, age):
+        """Constructor.
+
+        Args:
+            first_name (str) - First name of Student
+            last_name (str) - Last name of Student
+            age(int) - Age of Student
+
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """returns dictionary repr of this instance"""
+        """returns dictionary repr of Student object"""
         return self.__dict__
