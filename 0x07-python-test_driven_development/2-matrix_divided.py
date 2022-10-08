9e1b58c344a211d7c7b3929a07aff37dfd8931be
Roncollins MM <muriukiron@gmail.com>
2022-10-08 12:27:09 +0300

    Function that divides a matrix + unit tests

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
 """
-Module matrix_divided
-Divides each element of a matrix of numbers by a number
+Module that divides each element of a matrix.
 """
 
 
 def matrix_divided(matrix, div):
-    """Returns a new matrix (list of list)
-    with the result of the division of matrix by div
-    rounded to 2 decimal places.
+    """Divides all elements of a matrix by an equal divisor to the nearest 2
+    decimal places.
+
+    Args:
+        matrix - The matrix
+        div - The divisor
+
+    Returns:
+        A new matrix of the elements after division
+
+    Raises:
+        TypeError:
+            If the matrix elements are not of type ``int`` or ``float``.
+            If the rows of the matrix are not of the same size.
+        ZeroDivisionError:
+            If div == 0
     """
 
     if not isinstance(matrix, list) or len(matrix) == 0 or not matrix[0]:
@@ -27,6 +39,8 @@ def matrix_divided(matrix, div):
     len_rows = []
     for row in matrix:
         len_rows.append(len(row))
+
+    # This.. THIS is why I love Python !!!!!
     if not all(elem == len_rows[0] for elem in len_rows):
         raise TypeError("Each row of the matrix must have the same size")
 
@@ -36,6 +50,7 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
+    # This is also why I love Python... such beauty in a single line :`)
     new_matrix = [[round(x / div, 2) for x in row] for row in matrix]
 
     return new_matrix
