ccbdeb9762351405dce8d778b6d291f14ae5c00d
Roncollins MM <muriukiron@gmail.com>
2022-10-08 09:50:27 +0300

    Function that returns attrubutes of its objects added to Student class

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-"""this module creates a student class"""
+"""Student class that """
 
 
 class Student:
-    """this is the student class"""
+    """Constructor"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """returns dictionary repr of this instance"""
+        """Retrieves the attributes specified from the __dict__ representation
+        of an instance of the Student class. If no specific attributes are
+        requested, all attributes are retrieved.
+
+        Args:
+            attrs - The attributes(keys) to be retrived
+
+        Returns:
+            A dictionary object containing the attributes retrieved
+
+        """
         thisdict = dict()
         if type(attrs) is list:
             for i in list(self.__dict__):
