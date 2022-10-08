e916b316da3fcd0372be319c3b8c6c9537ba01f8
Roncollins MM <muriukiron@gmail.com>
2022-10-08 08:48:39 +0300

    A module that converts an object to JSON and writes to file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """writes serialized text to a file"""
 
-
 import json
 
-
 def save_to_json_file(my_obj, filename):
-    """writes serialized text to a file"""
+    """Converts and object to JSON and writes to a file
+
+    Args:
+        my_obj - The object to be converted
+        filename - The file to be written to
+
+    """
     with open(filename, "w", encoding="UTF-8") as fi:
         fi.write(json.dumps(my_obj))
