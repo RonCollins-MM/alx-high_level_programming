9c6ecc53edf65ef143db3099013797f1207303fa
Roncollins MM <muriukiron@gmail.com>
2022-10-08 08:37:50 +0300

    A function that serializes an object and returns it

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 """this module serializes a string"""
 
-
 import json
 
-
 def to_json_string(my_obj):
-    """does the real stuff"""
+    """This function returns the JSON representation of an object(string)
+
+    Args:
+        my_object: Object(string) to be serialized
+
+    Returns:
+        The JSON representation of the object(string)
+
+    """
     return json.dumps(my_obj)
