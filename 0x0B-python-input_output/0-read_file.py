218795349a6a843b4a93cc56b0fe8df6fcc2a0c5
Roncollins MM <muriukiron@gmail.com>
2022-10-08 08:04:26 +0300

    A function that reads from a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""this module reads from a file"""
+"""Module with a function that reads from a file"""
 
 
 def read_file(filename=""):
-    """this functions reads a file"""
+    """this functions reads from a file"""
 
     with open(filename, "r", encoding="UTF-8") as thisfile:
         print(thisfile.read(), end="")
