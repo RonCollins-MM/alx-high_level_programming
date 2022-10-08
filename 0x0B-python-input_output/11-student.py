a5ee718382903c653cfb4a638f1380db4aa9da0b
Roncollins MM <muriukiron@gmail.com>
2022-10-08 10:02:16 +0300

    Function that modifies instance from JSON added

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-"""this module creates a student class"""
-
+"""Module with Student class that can be converted to JSON or modified from a JSON"""
 
 class Student:
-    """this is the student class"""
+    """Student class implementation"""
+
     def __init__(self, first_name, last_name, age):
+        """Constructor
+
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """returns dictionary repr of this instance"""
+        """Retrieves attributes from an instance of this class and returns a
+        dictionary object of the attributed.
+
+        See ``10-student.py`` for more details.
+        """
         thisdict = dict()
         if type(attrs) is list:
             for i in list(self.__dict__):
@@ -22,7 +29,13 @@ class Student:
             return self.__dict__
 
     def reload_from_json(self, json):
-        """reloads from json"""
+        """Changes the attributes of an instance of this class from a JSON
+        string.
+
+        Args:
+            json - The json file to read from.
+
+        """
         for i in list(self.__dict__):
             if i in list(json):
                 self.__dict__[i] = json[i]
