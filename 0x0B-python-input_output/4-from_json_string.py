e13396db4580e8224cba27edb595003d1c5d70bb
Roncollins MM <muriukiron@gmail.com>
2022-10-08 08:44:32 +0300

    A function that converts a JSON string to object

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-"""this deserializes a string"""
-
+"""Module that deserialises a string"""
 
 import json
 
-
 def from_json_string(my_str):
-    """does the real stuff"""
+    """Converts a JSON string to an object(Python Data Structure)
+
+    Args:
+        my_str - The JSON string to convert
+
+    Returns:
+        An object(Python data structure)
+
+    """
     return(json.loads(my_str))
