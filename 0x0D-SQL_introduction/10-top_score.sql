9b4f18b694c55f9ce6bfe2c418456f5fa0947596
Roncollins MM <muriukiron@gmail.com>
2022-10-17 17:41:38 +0300

    getting all records and ordering them

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,2 @@
--- lists all records of the table second_table
-SELECT score, name
-FROM second_table
-ORDER BY score DESC
+-- View records from second table
+SELECT score, name FROM second_table ORDER BY score DESC;
