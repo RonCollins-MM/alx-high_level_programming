3e72a7b062ec00ee9a17ca7d7adfc2a83c3613e7
Roncollins MM <muriukiron@gmail.com>
2022-10-27 19:50:05 +0300

    Opening URL from within python

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Script that fetches https://alx-intranet.hbtn.io/status and displays a
+formatted output
+"""
+
+from urllib import request
+
+if __name__ == "__main__":
+    with request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        html = response.read()
+        print('Body response:')
+        print(f'\t- type: {type(html)}')
+        print(f'\t- content: {html}')
+        print(f'\t- utf8 content: {html.decode("utf-8")}')
