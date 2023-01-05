0e20c70c635aaf3dac1517845166b09cd4d941b8
Roncollins MM <muriukiron@gmail.com>
2023-01-05 09:19:19 +0300

    refactor: make code semistandard compliant

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,10 @@
+$(function () {
+  $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data, status) {
+    if (status === 'success') {
+      const films = data.results;
+      for (const i in films) {
+        $('#list_movies').append('<li>' + films[i].title + '</li>');
+      }
+    }
+  });
+});
